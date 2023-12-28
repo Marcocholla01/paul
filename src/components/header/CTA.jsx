d8f436f1d66781ef7136a2db0fe8651d634827d1
Marcocholla01 <marcochollapaul01@gmail.com>
2023-12-28 03:52:16 +0300

    Updated Resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,19 @@
-import React from "react"
-import CV from '../../assets/cv.pdf'
+import React from "react";
+import CV from "../../assets/Paul Ouma Ocholla Resume.pdf";
 //import "./cta.css";
 
-const CTA = () =>{
-    return(
-        <div className='cta'>
-            <a href={CV} download className='btn'> Download CV </a>
-            <a href='#contact' className='btn btn-primary'>Let's Talk</a>
-        </div>
-    )
-}
+const CTA = () => {
+  return (
+    <div className="cta">
+      <a href={CV} download className="btn">
+        {" "}
+        Download CV{" "}
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's Talk
+      </a>
+    </div>
+  );
+};
 
 export default CTA;
